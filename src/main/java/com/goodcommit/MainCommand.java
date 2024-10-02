5d2d2a0b9868c2184ee6c0cc7c549e6d2738ea70
Guhan <74915094+Gukkey@users.noreply.github.com>
2024-10-02 23:12:22 +0530

    Parity/git commit (#4)
    * perf (main command): new methods (getDescription, getBreakingChanges)
    moved the messy part of getting description and breaking changes into separate methods for code readability
    
    * build (pom.xml): added slf4j dependencies
    Added slf4j dependencies for logging
    
    * chore (releases): replaced the .exe
    replaced .exe file to resolve the 'This application requires JRE' message if an user tries to run the cli
    
    * refactor: added logging, did a little optimisation
    added logging, created new method to handle commit message string concatination
    BREAKING CHANGE: logging, new methods
    
    * build (.gitignore): added /manifest folder
    note: manifest folder is just the metadata folder for the winget config for the CLI
    
    * build: added config for logger, add a dep on dependency-reduced-pom
    
    * build (pom.xml): removed unwanted deps
    
    * feat (MainCommand): Added parity for git commit's flag and other cool stuff
    added parity support for git commit command's flags, added --safe flag letting users see the commit message before committing, little bit optimisation in the middle
    
    * build (pom.xml): added a new dep: slf4j-nop
    
    * refactor: using safe mode enables users to edit their generated commit messages now
    
    * refactor: fixed logging, fixed logo appearing when help is prompted
    
    * build: updated version number
    
    * refactor: moved the helper functions to a separate class file
    
    * replaced the loggers with proper print statements for commit process results

--- src/main/java/com/goodcommit/MainCommand.java
+++ src/main/java/com/goodcommit/MainCommand.java
@@ -1,120 +1,124 @@
 package com.goodcommit;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
-import org.jline.utils.AttributedString;
-import org.jline.utils.AttributedStyle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-@Command(name = "", description = "Good commit, a CLI alternative for commitzen")
+import static com.goodcommit.helpers.Helpers.getBreakingChanges;
+import static com.goodcommit.helpers.Helpers.getDescription;
+import static com.goodcommit.helpers.Helpers.getString;
+import static com.goodcommit.helpers.Helpers.selectScopeType;
+import static com.goodcommit.helpers.Helpers.validateArgs;
+
+@Command(name = "", mixinStandardHelpOptions = true, description = "Good commit, a CLI alternative for commitzen")
 public class MainCommand implements Runnable {
 
-  final String logo = 
+  private static final Logger LOGGER = LoggerFactory.getLogger(MainCommand.class);
+
+  private static final String LOGO =
   " ██████╗  ██████╗  ██████╗ ██████╗      ██████╗ ██████╗ ███╗   ███╗███╗   ███╗██╗████████╗     ██████╗██╗     ██╗\n" +
   "██╔════╝ ██╔═══██╗██╔═══██╗██╔══██╗    ██╔════╝██╔═══██╗████╗ ████║████╗ ████║██║╚══██╔══╝    ██╔════╝██║     ██║\n" +
   "██║  ███╗██║   ██║██║   ██║██║  ██║    ██║     ██║   ██║██╔████╔██║██╔████╔██║██║   ██║       ██║     ██║     ██║\n" +
   "██║   ██║██║   ██║██║   ██║██║  ██║    ██║     ██║   ██║██║╚██╔╝██║██║╚██╔╝██║██║   ██║       ██║     ██║     ██║\n" +
   "╚██████╔╝╚██████╔╝╚██████╔╝██████╔╝    ╚██████╗╚██████╔╝██║ ╚═╝ ██║██║ ╚═╝ ██║██║   ██║       ╚██████╗███████╗██║\n" +
   " ╚═════╝  ╚═════╝  ╚═════╝ ╚═════╝      ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚═╝╚═╝   ╚═╝        ╚═════╝╚══════╝╚═╝\n" +
-  "                                                                                                                 ";
+  "                                                                                                                                                                   ";
 
-  final String scopePrompt = "Enter the type of the scope\n"
-      + "1. build: Changes that affect the build system or external dependencies (example"
-      + " scopes: gulp, broccoli, npm) \n"
-      + "2. ci: Changes to CI configuration files and scripts (example scopes: Travis, Circle,"
-      + " BrowserStack, SauceLabs) \n"
-      + "3. chore: Changes which doesn't change source code or tests (e.g. changes to the build"
-      + " process, auxiliary tools, libraries) \n"
-      + "4. docs: Documentation only changes \n"
-      + "5. feat: A new feature \n"
-      + "6. fix: A bug fix \n"
-      + "7. perf: A code change that improves performance \n"
-      + "8. refactor: A code change that neither fixes a bug nor adds a feature \n"
-      + "9. style: Changes that do not affect the meaning of the code (white-space, formatting,"
-      + " missing semi-colons, etc) \n"
-      + "10. test: Adding missing tests or correcting existing tests \n"
-      + "Enter the scope number: ";
-
-  final String[] scopes = {
-      "build", "ci", "chore", "docs", "feat", "fix", "perf", "refactor", "style", "test"
-  };
-  
-  final String helpMessage = "For Good commit cli to work, you just have to type \"good-commit\" and the process will"
-      + " start and the CLI will ask necessary questions for the commit. If you want to speed"
-      + " up the process and give some manual inputs via options, here are they.\n"
+  private static final String HELP_MESSAGE = "For Good commit cli to work, you just have to type \"good-commit\" and the process will"
+      + " start and the CLI will ask necessary prompts for the commit. If you want to speed"
+      + " up the process and give some manual inputs via options, below are the options. You can add the git flags after these flags like \"good-commit <good-commit-flags> <git commit flags>\" \n"
       + //
       "\n"
       + //
-      "-t, --type: scope type\n"
+      "     --type              scope type\n"
       + //
-      "-s, --scope: scope\n"
-      + //
-      "\n"
-      + //
-      "-d, --description: commit description\n"
-      + //
-      "-m, --message: commit message\n"
+      "     --scope             scope\n"
       + //
       "\n"
       + //
-      "-b, --breaking-changes: breaking changes description\n"
+      "     --description       commit description\n"
       + //
-      "-a, --draw-attention: draw attention\n"
+      "     --message           commit message\n"
       + //
-      "-i, --issue-references: (github) issue references\n"
+      "\n"
+      + //
+      "     --breaking-changes  breaking changes description\n"
+      + //
+      "     --draw-attention    draw attention\n"
+      + //
+      "     --issue-references  issue references\n"
+      + //
+      "     --safe              safe mode\n"
       + //
       "\n"
       + //
       "Reminder: These options are not mandatory. The CLI will ask inputs for these if you"
       + " initalize it via \"good-commit\" command.";
 
-  @Option(names = { "-t", "--type" }, arity = "0..1", description = "Scope type")
-  String scopeType;
+  @Option(names = { "--scope-type" }, arity = "0..1", description = "Scope type")
+  private String scopeType;
 
-  @Option(names = { "-s", "--scope" }, arity = "0..1", description = "Scope")
-  String scope;
+  @Option(names = { "--scope" }, arity = "0..1", description = "Scope")
+  private String scope;
 
-  @Option(names = { "-d", "--description" }, arity = "0..1", description = "Commit description")
-  String description;
+  @Option(names = { "--description" }, arity = "0..1", description = "Commit description")
+  private String description;
 
-  @Option(names = { "-m", "--message" }, arity = "0..1", description = "Commit message")
-  String message;
+  @Option(names = { "--commit-message" }, arity = "0..1", description = "Commit message")
+  private String message;
 
-  @Option(names = { "-b", "--breaking-changes" }, arity = "0..1", description = "Breaking changes")
-  String breakingChanges;
+  @Option(names = { "--breaking-changes" }, arity = "0..1", description = "Breaking changes")
+  private String breakingChanges;
 
-  @Option(names = { "-a", "--draw-attention" }, arity = "0..1", description = "Draw Attention")
-  boolean drawAttention;
+  @Option(names = { "--draw-attention" }, arity = "0..1", description = "Draw Attention")
+  private boolean drawAttention;
 
-  @Option(names = { "-i", "--issue-references" }, arity = "0..1", description = "Issue Reference(s)")
-  String issueReferences;
+  @Option(names = { "--issue-references" }, arity = "0..1", description = "Issue Reference(s)")
+  private String issueReferences;
 
-  @Option(names = { "-h", "--help" }, description = "Display help message")
-  boolean help = false;
+  @Option(names = { "--safe" }, arity = "0..1", description = "Safe mode, whether to display the commit message before commiting")
+  private boolean safeMode = true;
+
+  @Option(names = { "--help" }, description = "Display help message")
+  private boolean help;
+
+  @Parameters(description = "Git flags", arity = "0..*")
+  private String[] gitFlags = new String[0];
+  private Scanner sc = new Scanner(System.in);
 
   @Override
   public void run() {
-    System.out.println("\n" + logo + "A CLI alternative for commitzen \n");
-    Scanner sc = new Scanner(System.in);
+
     if (help) {
-      System.out.println(helpMessage);
+      System.out.println(HELP_MESSAGE);
       sc.close();
       return;
     }
+
+    System.out.println("\n\n" + LOGO + "\n\nA CLI alternative for commitzen \n");
+
     if (scopeType == null) {
       try {
         scopeType = selectScopeType();
+        LOGGER.info("Scope Type: {}", scopeType);
       } catch (IOException e) {
-        System.out.println("Error reading input: " + e.getMessage());
+        LOGGER.error("Error reading input: {}", e.getMessage());
         sc.close();
         return;
       }
     }
+
     if (scope == null) {
       System.out.print(
           "What is the scope of this change (e.g. component or file name): (press enter to skip):"
@@ -123,176 +127,96 @@ public class MainCommand implements Runnable {
     }
 
     if (description == null) {
-      System.out.print(
-          "Enter the commit description, the description is a short summary of the code changes"
-              + " (maximum 92 characters): ");
-      description = sc.nextLine();
-      while (description.length() > 92 || description.length() == 0) {
-        System.out.print(
-            "The description is too long or empty, please enter a valid description: ");
-        description = sc.nextLine();
-      }
+      description = getDescription();
+      LOGGER.info("Description: {}", description);
     }
+
     if (message == null) {
       System.out.print(
           "Enter the commit message, the message is a longer description of the commit (press enter"
               + " to skip): ");
       message = sc.nextLine();
     }
+
     if (breakingChanges == null) {
-      System.out.print("Does this commit have breaking changes? (y/n): ");
-      String hasBreakingChanges = sc.nextLine();
-      if (hasBreakingChanges.isEmpty()) {
-        hasBreakingChanges = "n";
-      }
-      while (!(hasBreakingChanges.equals("y") || hasBreakingChanges.equals("n"))) {
-        System.out.print("Invalid input, please enter y or n: ");
-        breakingChanges = sc.nextLine();
-      }
-      if (hasBreakingChanges.equals("y")) {
-        System.out.print("Draw Attention? (y/n): ");
-        drawAttention = sc.nextLine().equals("y");
-        if (message.isEmpty() && !drawAttention) {
-          System.out.print(
-              "A BREAKING CHANGE commit requires a body. Please enter a longer description of the"
-                  + " commit itself: ");
-          message = sc.nextLine();
-          while (message.isEmpty()) {
-            System.out.print("The message is empty, please enter a valid message: ");
-            message = sc.nextLine();
-          }
-        }
-        System.out.print("Describe the breaking changes: ");
-        breakingChanges = sc.nextLine();
-        while (breakingChanges.isEmpty() && !drawAttention) {
-          System.out.print(
-              "The breaking changes is empty, please enter a valid breaking changes: ");
-          breakingChanges = sc.nextLine();
-        }
-      }
-      if (issueReferences == null) {
-        System.out.print("Enter the issue reference(s) (press enter to skip): ");
-        issueReferences = sc.nextLine();
-        sc.close();
+      try {
+        breakingChanges = getBreakingChanges(message, drawAttention);
+      } catch (IOException e) {
+        e.printStackTrace();
       }
     }
-    String commit = "";
-    if (scopeType != null && !scopeType.isEmpty()) {
-      commit += scopeType;
+
+    if (issueReferences == null) {
+      System.out.print("Enter the issue reference(s) (press enter to skip): ");
+      issueReferences = sc.nextLine();
     }
-    if (scope != null && !scope.isEmpty()) {
-      commit += " (" + scope + ")";
+
+    String commit = getString(scopeType, scope, drawAttention, description, message, breakingChanges, issueReferences);
+    LOGGER.info(commit);
+
+    if (safeMode) {
+      try (Terminal terminal = TerminalBuilder.terminal()) {
+        LineReader lineReader = LineReaderBuilder.builder().terminal(terminal).build();
+        String prompt = "\n\nBelow is the generated commit message, please make changes if you want or press ENTER to continue to start the commit process\n";
+        commit = lineReader.readLine(prompt, null, commit);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
-    if (drawAttention) {
-      commit += "!";
-    }
-    if (description != null && !description.isEmpty()) {
-      commit += ": " + description + "\n";
-    }
-    if (message != null && !message.isEmpty()) {
-      commit += message + "\n";
-    }
-    if (breakingChanges != null && !breakingChanges.isEmpty()) {
-      commit += "BREAKING CHANGE: " + breakingChanges + "\n";
-    }
-    if (issueReferences != null && !issueReferences.isEmpty()) {
-      commit += issueReferences;
-    }
+
+    sc.close();
     Process gitProcess;
+
     try {
-      gitProcess = new ProcessBuilder("git", "commit", "-m", commit).start();
+      StringBuilder sb = new StringBuilder();
+
+      for (String s : gitFlags) {
+        sb.append(s + " ");
+      }
+
+      String gitFlagsString = sb.toString().trim();
+      commit = commit.trim();
+
+      List<String> processBuilderList = new ArrayList<>();
+      processBuilderList.add("git");
+      processBuilderList.add("commit");
+      processBuilderList.add("-m");
+      processBuilderList.add(commit);
+
+      LOGGER.info("SB LENGTH: {}", sb.length());
+      if (sb.length() != 0) {
+        processBuilderList.add(gitFlagsString);
+      }
+
+      gitProcess = new ProcessBuilder(processBuilderList).start();
       gitProcess.waitFor();
       String res = new String(gitProcess.getInputStream().readAllBytes());
-      System.out.println(res);
+      System.out.println("\n" + res);
+
       if (gitProcess.exitValue() == 0) {
-        System.out.println("Commit created successfully");
+        LOGGER.info("Commit created successfully");
+        System.out.println(String.format(
+            "Commit has been created successfully. Commit message: %n%s%nIncase if you want to edit the commit message use the following, git commit --amend -m \"New commit message\"",
+            commit));
       } else {
-        System.out.println(new String(gitProcess.getErrorStream().readAllBytes()));
+        String errorMessage = new String(gitProcess.getErrorStream().readAllBytes());
+        System.out.println("Error while trying to create commit: " + errorMessage);
       }
     } catch (IOException | InterruptedException e) {
-      System.out.println("Failed to create commit");
-      e.printStackTrace();
+      System.out.println("Failed to create commit: %s" + e.getMessage());
     }
   }
 
   public static void main(String[] args) {
-    new CommandLine(new MainCommand()).execute(args);
-  }
-
-  private String selectScopeType() throws IOException {
-    Terminal terminal = null;
-    try {
-      terminal = TerminalBuilder.builder().system(true).build();
-    } catch (Exception e) {
-      System.out.println("Failed to create terminal");
-      e.printStackTrace();
-    }
-
-    String[] options = {
-        "build: Changes that affect the build system or external dependencies",
-        "ci: Changes to CI configuration files and scripts",
-        "chore: Changes which doesn't change source code or tests",
-        "docs: Documentation only changes",
-        "feat: A new feature",
-        "fix: A bug fix",
-        "perf: A code change that improves performance",
-        "refactor: A code change that neither fixes a bug nor adds a feature",
-        "style: Changes that do not affect the meaning of the code",
-        "test: Adding missing tests or correcting existing tests"
-    };
-
-    if (terminal != null) {
-      return interactiveSelect(terminal, options);
+    if (!validateArgs(args)) {
+      System.out.println(
+          "Wrong arguments, please go through help message so see how to construct proper args by entering \"git commit --help\"");
+      System.exit(0);
     } else {
-      throw new RuntimeException("Failed to create terminal");
-    }
-  }
-
-  private String interactiveSelect(Terminal terminal, String[] options) {
-    int selected = 0;
-    while (true) {
-      terminal.puts(org.jline.utils.InfoCmp.Capability.clear_screen);
-      terminal.flush();
-      terminal
-          .writer()
-          .println(
-              "\n"
-                  + logo
-                  + "\nA CLI alternative for commitzen \n"
-                  + "\nSelect the type of scope: ");
-      for (int i = 0; i < options.length; i++) {
-        if (i == selected) {
-          terminal
-              .writer()
-              .println(
-                  new AttributedString(
-                      "> " + options[i],
-                      AttributedStyle.DEFAULT.foreground(AttributedStyle.GREEN))
-                      .toAnsi());
-        } else {
-          terminal.writer().println(" " + options[i]);
-        }
-      }
-      terminal.writer().println("\nUse arrow keys to navigate, Enter to select");
-      terminal.flush();
-
-      // Read input
-      int c;
-      try {
-        c = terminal.reader().read();
-        switch (c) {
-          case 65: // Up arrow
-            selected = (selected - 1 + options.length) % options.length;
-            break;
-          case 66: // Down arrow
-            selected = (selected + 1) % options.length;
-            break;
-          case 13: // Enter
-            return scopes[selected];
-        }
-      } catch (IOException e) {
-        e.printStackTrace();
-      } 
+      int exitCode = new CommandLine(new MainCommand())
+          .setUnmatchedOptionsArePositionalParams(true)
+          .execute(args);
+      System.exit(exitCode);
     }
   }
 }
